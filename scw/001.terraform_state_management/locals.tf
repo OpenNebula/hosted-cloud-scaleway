b3b7f1f582b6a5fc3d48cf5d370ff1735a9f63b2
fdelrieu <fdelrieu@kaliop.com>
2025-07-22 10:59:04 +0200

    tf init project + bucket

--- scw/001.terraform_state_management/locals.tf
+++ scw/001.terraform_state_management/locals.tf
@@ -1,3 +1,4 @@
 locals {
   scw_infrastructure_project_name = "${var.customer_name}-${var.project_name}-${var.state_infrastructure_information.scw_infrastructure_project_name}"
+  scw_state_bucket = "${local.scw_infrastructure_project_name}-tfstates"
 }
---
+++ scw/001.terraform_state_management/provider.tf
@@ -0,0 +1,3 @@
+# Configure the Scaleway provider
+provider "scaleway" {
+}
