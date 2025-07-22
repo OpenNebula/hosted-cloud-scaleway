4e994a214a7636171e5383170232644a9905da97
fdelrieu <fdelrieu@kaliop.com>
2025-07-22 10:59:04 +0200

    tf init project + bucket

--- scw/001.terraform_state_management/variables.tf
+++ scw/001.terraform_state_management/variables.tf
@@ -10,11 +10,6 @@ variable "project_name" {
 
 variable "state_infrastructure_information" {
   type = object({
-    scw_profile                     = string
-    scw_state_bucket                = string
-    scw_state_region                = string
-    scw_state_zone                  = string
-    scw_organization_id             = string
     scw_infrastructure_project_name = string
   })
 }
